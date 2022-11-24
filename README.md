eef79dfbb882b8a71144a0f50b150e9fe97534d5
macinjoke <shunji.makino@gmail.com>
2022-11-24 18:31:36 +0900

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-vite
+vite試しに触ったやつ。
+
+↓ここらへんまで実装
+https://reactrouter.com/en/main/start/tutorial#creating-contacts
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    action: rootAction,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
